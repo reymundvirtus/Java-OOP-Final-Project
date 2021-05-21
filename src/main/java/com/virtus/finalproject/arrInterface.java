2d17373376ddc67497d71194ed76233f8355f3cd
reymundvirtus <reymundvirtus@gmail.com>
2021-05-21 11:38:43 +0800

    Sorting Tool for Array & Insertion LinkedList

---
+++ src/main/java/com/virtus/finalproject/arrInterface.java
@@ -0,0 +1,11 @@
+package com.virtus.finalproject;
+
+// this interface is for array only
+public interface arrInterface {
+    // in interface this methods here is the behavior of our program
+    public void selectMethod();
+    public void whatToDo();
+    public void oneDim();
+    public void twoDim();
+    public void threeDim();
+}
