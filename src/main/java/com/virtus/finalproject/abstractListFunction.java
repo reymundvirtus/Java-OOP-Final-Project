956bb403b55fe5df55dc03da01348979f477179b
reymundvirtus <reymundvirtus@gmail.com>
2021-05-21 11:39:19 +0800

    Sorting Tool for Array & Insertion LinkedList

---
+++ src/main/java/com/virtus/finalproject/abstractListFunction.java
@@ -0,0 +1,11 @@
+package com.virtus.finalproject;
+
+// this abstract is only for linkedliest
+public abstract class abstractListFunction {
+    // we hide the process invoke in linkedkist
+    abstract void getData();
+    abstract void insertData();
+    abstract void clearList();
+    abstract void setter(int newData);
+    abstract int getter();
+}
